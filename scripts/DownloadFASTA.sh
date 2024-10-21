938a179eff241156aa4c84c65ac7db5aa19426cb
Rita Ferrolho <ritaferrolho@ua.pt>
2024-10-21 11:14:53 +0100

    add complete human genome url

--- scripts/DownloadFASTA.sh
+++ scripts/DownloadFASTA.sh
@@ -16,31 +16,33 @@ function SHOW_HELP() {
 #
 # ===========================================================================
 #
-defaultIDs=(
+defaultUrls=(
 
-    "NC_058373.1" # Felis catus isolate Fca126 chromosome B3, F.catus_Fca126_mat1.0, whole genome shotgun sequence (144MB)
+    "https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz" # complete human genome (~3GB)
 
-    "NC_000001.11" # complete chromosome 1 from Homo sapiens
-    "NC_000008.11" # complete chromosome 8 from Homo sapiens
-    "NC_000021.9" # complete chromosome 21 from Homo sapiens
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_058373.1&rettype=fasta&retmode=text" # Felis catus isolate Fca126 chromosome B3, F.catus_Fca126_mat1.0, whole genome shotgun sequence (144MB)
 
-    "NC_000024.1" # CY
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000001.11&rettype=fasta&retmode=text" # complete chromosome 1 from Homo sapiens
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000008.11&rettype=fasta&retmode=text" # complete chromosome 8 from Homo sapiens
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000021.9&rettype=fasta&retmode=text" # complete chromosome 21 from Homo sapiens
 
-    "NC_000908.2" # Mycoplasmoides genitalium G37, complete sequence
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000024.1&rettype=fasta&retmode=text" # CY
 
-    "BA000046.3" # Pan troglodytes DNA, chromosome 22, complete sequence (32 MB)
-    "NC_073246.2" # Gorilla gorilla gorilla isolate KB3781 chromosome 22, NHGRI_mGorGor1-v2.0_pri, whole genome shotgun sequence (40MB)
-    "NC_072005.2" # Pongo abelii isolate AG06213 chromosome 20, NHGRI_mPonAbe1-v2.0_pri, whole genome shotgun sequence (63M)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000908.2&rettype=fasta&retmode=text" # Mycoplasmoides genitalium G37, complete sequence
 
-    "NC_004461.1" # Staphylococcus epidermidis ATCC 12228, complete sequence (staphylococcus_epidermidis_raw.fa) (2,4M)
-    "CM029732.1" # Pollicipes pollicipes isolate AB1234 mitochondrion, complete sequence, whole genome shotgun sequence (mt_genome_CM029732_raw.fa) (15KB)
-    "OM812693.1" # covid (SARS_CoV_Hun_1_raw.fa) (30K)
-    "CM047480.1" # Aldabrachelys gigantea (290M)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=BA000046.3&rettype=fasta&retmode=text" # Pan troglodytes DNA, chromosome 22, complete sequence (32 MB)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_073246.2&rettype=fasta&retmode=text" # Gorilla gorilla gorilla isolate KB3781 chromosome 22, NHGRI_mGorGor1-v2.0_pri, whole genome shotgun sequence (40MB)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_072005.2&rettype=fasta&retmode=text" # Pongo abelii isolate AG06213 chromosome 20, NHGRI_mPonAbe1-v2.0_pri, whole genome shotgun sequence (63M)
 
-    "KT868810.1" # Cutavirus strain BR-283 NS1 gene, partial cds; and putative VP1, hypothetical protein, VP2, and hypothetical protein genes, complete cds (4,3K)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_004461.1&rettype=fasta&retmode=text" # Staphylococcus epidermidis ATCC 12228, complete sequence (staphylococcus_epidermidis_raw.fa) (2,4M)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=CM029732.1&rettype=fasta&retmode=text" # Pollicipes pollicipes isolate AB1234 mitochondrion, complete sequence, whole genome shotgun sequence (mt_genome_CM029732_raw.fa) (15KB)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=OM812693.1&rettype=fasta&retmode=text" # covid (SARS_CoV_Hun_1_raw.fa) (30K)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=CM047480.1&rettype=fasta&retmode=text" # Aldabrachelys gigantea (290M)
 
-    "NC_000898.1" # Human herpesvirus 6B, complete genome (161K)
-    "NC_001664.4" # Human betaherpesvirus 6A, variant A DNA, complete virion genome, isolate U1102 (158K)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=KT868810.1&rettype=fasta&retmode=text" # Cutavirus strain BR-283 NS1 gene, partial cds; and putative VP1, hypothetical protein, VP2, and hypothetical protein genes, complete cds (4,3K)
+
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000898.1&rettype=fasta&retmode=text" # Human herpesvirus 6B, complete genome (161K)
+    "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_001664.4&rettype=fasta&retmode=text" # Human betaherpesvirus 6A, variant A DNA, complete virion genome, isolate U1102 (158K)
 )
 #
 while [[ $# -gt 0 ]]; do
@@ -51,7 +53,7 @@ while [[ $# -gt 0 ]]; do
         shift 2; 
         ;;
     -id)
-        ids+=("$2")
+        urls+=("$2")
         shift 2; 
         ;;
     *) 
@@ -61,8 +63,8 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 #
-# default ids are only considered if no ncbi id is defined by user
-[ "${#ids[@]}" -eq 0 ] && ids=( "${defaultIDs[@]}" )
+# default urls are only considered if no ncbi id is defined by user
+[ "${#urls[@]}" -eq 0 ] && urls=( "${defaultUrls[@]}" )
 #
 configJson="../config.json"
 rawSequencesPath="$(grep 'rawSequencesPath' $configJson | awk -F':' '{print $2}' | tr -d '[:space:],"' )";
@@ -70,10 +72,10 @@ mkdir -p $rawSequencesPath;
 #
 # === Download rawFiles ===========================================================================
 #
-printf "downloading ${#ids[@]} sequence files...\n"
-for id in "${ids[@]}"; do
+printf "downloading ${#urls[@]} sequence files...\n"
+for url in "${urls[@]}"; do
     #
-    url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=$id&rettype=fasta&retmode=text"
+    id=$(echo "$url" | awk -F'&id' '{print $2}' | awk -F'&' '{print $1}' | tr -d '\\=')
     rawFile="$(echo "${id}_raw" | tr '.' '_').fa"
     #
     if [[ ! -f "$rawSequencesPath/$rawFile" ]]; then 
